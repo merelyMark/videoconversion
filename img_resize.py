2742e043980fdc228ae1b9a3a76f543d21168ffb
Mark Kim <mbk@cs.utah.edu>
2018-07-15 15:40:04 -0400

    fix date time

--- img_resize.py
+++ img_resize.py
@@ -1,6 +1,36 @@
-import os
+import os, time
+from stat import *
+
 import sys
 from PIL import Image
+
+TEST_DATETIME = False
+
+def fix_datetime(filePath, outfile):
+    in_st = os.stat(filePath)
+    in_atime = in_st[ST_ATIME]
+    in_mtime = in_st[ST_MTIME]
+
+    out_st = os.stat(outfile)
+    out_atime = out_st[ST_MTIME]
+    print (time.ctime(out_atime) + ' ' + time.ctime(in_mtime))
+    if (not TEST_DATETIME):
+        os.utime(outfile, (out_atime, in_mtime))
+
+def bulkFixdatetime(imageFolder):
+    imgExts = ["png", "bmp", "jpg"]
+    for path, dirs, files in os.walk(imageFolder):
+        for fileName in files:
+            ext = fileName[-3:].lower()
+            if ext not in imgExts:
+                continue
+
+            saveFilename = os.path.join(os.getcwd(), fileName)
+            try:
+                filePath = os.path.join(path, fileName)
+                fix_datetime(filePath, saveFilename)
+            except (OSError, IOError) as err:
+                print("Error, could not convert " + filePath + " " + str(err))
 
 def resize(filePath, factor, saveFilename):
     im = Image.open(filePath)
@@ -24,6 +54,7 @@ def bulkResize(imageFolder, factor):
                 try:
                     filePath = os.path.join(path, fileName)
                     resize(filePath, factor, saveFilename)
+                    fix_datetime(filePath, saveFilename)
                 except (OSError, IOError) as err:
                     print("Error, could not convert " + filePath + " " + str(err))
             else:
@@ -31,5 +62,6 @@ def bulkResize(imageFolder, factor):
 
 if __name__ == "__main__":
     imageFolder=sys.argv[1] # first arg is path to image folder
-    resizeFactor=float(sys.argv[2])/100.0# 2nd is resize in %
-    bulkResize(imageFolder, resizeFactor)
+    #resizeFactor=float(sys.argv[2])/100.0# 2nd is resize in %
+    #bulkResize(imageFolder, resizeFactor)
+    bulkFixdatetime(imageFolder)
