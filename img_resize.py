74e328530e6916b392dff240588deae71605ed50
Mark Kim <mbk@cs.utah.edu>
2018-07-15 21:31:44 -0400

    rotate when appropriat

--- img_resize.py
+++ img_resize.py
@@ -2,9 +2,50 @@ import os, time
 from stat import *
 
 import sys
-from PIL import Image
+from PIL import Image,  ExifTags
 
 TEST_DATETIME = False
+def fix_orientation(exifdata):
+
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation]=='Orientation':
+            break
+    if (exifdata != None):
+        exif=dict(exifdata.items())
+
+        if (orientation in exif):
+            if exif[orientation] == 3:
+                return 180
+            elif exif[orientation] == 6:
+                return 270
+            elif exif[orientation] == 8:
+                return 90
+    return 0
+
+
+def bulkFixOrientation(imageFolder):
+    imgExts = ["png", "bmp", "jpg"]
+    for path, dirs, files in os.walk(imageFolder):
+        for fileName in files:
+            ext = fileName[-3:].lower()
+            if ext not in imgExts:
+                continue
+
+            saveFilename = os.path.join(os.getcwd(), fileName)
+            if (not os.path.isfile(saveFilename)):
+                try:
+                    filePath = os.path.join(path, fileName)
+                    im = Image.open(filePath)
+                    if (hasattr(im, '_getexif')):    
+                        degrees = fix_orientation(im._getexif())
+                        im = im.rotate(degrees, expand=True)
+                    im.save(saveFilename)
+                    im.close()
+
+                except (OSError, IOError) as err:
+                    print("Error, could not convert " + filePath + " " + str(err))
+            else:
+                print(saveFilename + " already exists.")
 
 def fix_datetime(filePath, outfile):
     in_st = os.stat(filePath)
@@ -38,6 +79,10 @@ def resize(filePath, factor, saveFilename):
     w, h  = im.size
 
     newIm = im.resize((int(w*factor), int(h*factor)))
+    if (hasattr(im, '_getexif')):
+        degrees = fix_orientation(im._getexif())
+        print(degrees)
+        newIm = newIm.rotate(degrees, expand=True)
     # i am saving a copy, you can overrider orginal, or save to other folder
     #print(saveFilename) 
     newIm.save(saveFilename)
@@ -62,6 +107,7 @@ def bulkResize(imageFolder, factor):
 
 if __name__ == "__main__":
     imageFolder=sys.argv[1] # first arg is path to image folder
-    #resizeFactor=float(sys.argv[2])/100.0# 2nd is resize in %
-    #bulkResize(imageFolder, resizeFactor)
-    bulkFixdatetime(imageFolder)
+    resizeFactor=float(sys.argv[2])/100.0# 2nd is resize in %
+    bulkResize(imageFolder, resizeFactor)
+    #bulkFixdatetime(imageFolder)
+    #bulkFixOrientation(imageFolder)
